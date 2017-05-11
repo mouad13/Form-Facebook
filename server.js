7c6eb642b81b569c50616069c7909fc4ab0412f2
mouad13 <mouad.rifaielmoubarik@gmail.com>
2017-05-11 16:01:51 +0200

    add vision jade

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ server.connection({
     port: 3300 		
 });
 
-server.route({
-    method: 'POST',
-    path:'/signup', 
-    handler: function (request, reply) {
+// server.route({
+//     method: 'POST',
+//     path:'/signup', 
+//     handler: function (request, reply) {
 
-        return reply('youpiii');
-    }
-});
+//         return reply('youpiii');
+//     }
+// });
 
 
 server.register(require('inert'), (err) => {
@@ -48,4 +48,77 @@ server.register(require('inert'), (err) => {
 
 });
 
+
+
+server.register(require('vision'), function(err)  {
+
+    if (err) {
+        throw err;
+    }
+
+    server.views({
+        engines: { jade: require('jade') },
+        path: __dirname + '/views',
+        compileOptions: {
+            pretty: true
+        }
+    });
+
+    //server.route({ method: 'GET', path: '/', handler: rootHandler });
+   
+
+    server.route({
+    	method: 'POST',
+    	path:'/signup', 
+    	handler: function(request, reply){
+    		reply.view('index.jade', {formulaire: request.payload});
+    	}
+    }); 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var express = require('express');
+// var bodyParser = require('body-parser');
+
+
+// var app = express();
+
+
+// app.set('views', './views');
+// app.set('view engine', 'jade');
+
+// app.use(bodyParser.urlencoded({extended: false}));
+
+// app.use(express.static(__dirname+ '/'));
+
+
+// app.post('/signup', function(req, res){
+	
+
+// 	res.render('index.jade', { formulaire: req.body} );
+// 	console.log('youpiii');
+
+// });
+
+
+
+
+
+// app.listen(3300, function () {
+// 	console.log('posqkdpok');
+
+// })
 
